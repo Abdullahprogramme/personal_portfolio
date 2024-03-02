842826a3b0caaace47d247c3ce67bb72c06b9d6d
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-03-02 10:39:52 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Skills from './Components/Skills';
+import Work from './Components/Work';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
