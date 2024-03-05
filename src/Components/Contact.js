cec39c9340bd6cf67ae2e5308af2d5ce91253125
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-03-05 23:38:24 +0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -16,7 +16,7 @@ const sendEmail = (e) => {
 const Contact = () => {
     return (
         <>
-        <div name='contact' className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
+        <div name='contact' className="w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center p-4">
             <form onSubmit={sendEmail} className="flex flex-col mt-4 max-w-[600px] w-full">
                 <div>
                     <p className="text-4xl font-bold inline border-b-4 border-pink-300 text-[#416D19]">Contact</p>
@@ -27,12 +27,11 @@ const Contact = () => {
                 <textarea className='bg-[#ccd6f6] p-2 placeholder-black' name="message" rows="10" placeholder="Message"></textarea>
                 <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">Send</button>
             </form>
-        </div>
-        <div className="w-full bg-[#0a192f] flex justify-center items-center">
             <span className="font-bold text-2xl text-[#416D19]">
                 / Thank you for visiting my portfolio. I hope you liked it. /
             </span>
         </div>
+        
     </>
     );
     };
