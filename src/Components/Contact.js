f2072e213d61f659010c557e2d56f4ae4ecba05b
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-03-05 23:40:09 +0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
     return (
         <>
         <div name='contact' className="w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center p-4">
-            <form onSubmit={sendEmail} className="flex flex-col mt-4 max-w-[600px] w-full">
+            <form onSubmit={sendEmail} className="flex flex-col mt-10 max-w-[600px] w-full">
                 <div>
                     <p className="text-4xl font-bold inline border-b-4 border-pink-300 text-[#416D19]">Contact</p>
                     <p className="text-gray-300 py-4">/ Submit the form below or send me an email - abdtariq78@gmail.com /</p>
