b5cb26c8aedf78d01c523f51042dbb92e7ccc9bb
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-03-16 23:30:29 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {HiArrowNarrowRight} from 'react-icons/hi';
+import {Typed} from "react-typed";
 
 const Home = () => {
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ["Web Developer", "Coder", "Front-end developer", "Gamer", "Tech Enthusiast", "Learner"],
+            typeSpeed: 40,
+            backSpeed: 50,
+            loop: true,
+        });
+
+        // Cleanup
+        return () => {
+            typed.destroy();
+        };
+    }, []);
     return (
         <div name='home' className="w-full h-screen bg-[#0a192f]">
             
@@ -9,7 +26,7 @@ const Home = () => {
             <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
                 <p className="text-pink-600 font-bold text-2xl">Hi, My name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#416D19]">Abdullah Tariq</h1>
-                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a front-end developer.</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"><span ref={el} className="descriptionTyped" /></h2>
                 <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a front-end developer passionate about creating user-friendly interfaces. 
                     My expertise lies in crafting seamless and responsive web experiences. 
                     I bring ideas to life by blending creativity with technical know-how. 
