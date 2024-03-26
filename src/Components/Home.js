d68089230c2af2a8ee72f056e8e2fd37b8de1d80
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-03-26 10:33:47 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import {HiArrowNarrowRight} from 'react-icons/hi';
+import { FaArrowRight } from 'react-icons/fa';
 import {Typed} from "react-typed";
 
 const Home = () => {
@@ -27,11 +28,35 @@ const Home = () => {
                 <p className="text-pink-500 font-bold text-2xl">Hi, My name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#416D19]">Abdullah Tariq</h1>
                 <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"><span ref={el} className="descriptionTyped" /></h2>
-                <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a front-end developer passionate about creating user-friendly interfaces. 
-                    My expertise lies in crafting seamless and responsive web experiences. 
-                    I bring ideas to life by blending creativity with technical know-how. 
-                    Proficient in React and Python, I have prior experience in both technologies. 
-                    Let's collaborate to build something amazing! </p>
+
+                <div>
+                    <p className="text-[#8892b0] py-1 max-w-[700px]">
+                        <span className="flex items-center">
+                        <FaArrowRight className="mr-2 text-[#416D19]"  />I'm a front-end developer passionate about creating user-friendly interfaces.
+                        </span>
+                    </p>
+                    <p className="text-[#8892b0] py-1 max-w-[700px]">
+                        <span className="flex items-center">
+                        <FaArrowRight className="mr-2 text-[#416D19]"  />My expertise lies in crafting seamless and responsive web experiences. 
+                        </span>
+                    </p>
+                    <p className="text-[#8892b0] py-1 max-w-[700px]">
+                        <span className="flex items-center">
+                        <FaArrowRight className="mr-2 text-[#416D19]"  />I bring ideas to life by blending creativity with technical know-how. 
+                        </span>
+                    </p>
+                    <p className="text-[#8892b0] py-1 max-w-[700px]"> 
+                        <span className="flex items-center">
+                        <FaArrowRight className="mr-2 text-[#416D19]"  />Proficient in React and Python, I have prior experience in both technologies. 
+                        </span>
+                    </p>
+                    <p className="text-[#8892b0] py-1 max-w-[700px]">
+                        <span className="flex items-center">
+                        <FaArrowRight className="mr-2 text-[#416D19]"  /> Let's collaborate to build something amazing! 
+                        </span>
+                    </p>
+                </div>
+                 
 
                 <div>
                     <button className=" text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-500 hover:border-pink-500">View work
