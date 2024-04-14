d1e54c4963eeb02bb71aeaf405e01958ac492a1e
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-14 20:25:47 +0500

    Update Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -7,6 +7,7 @@ import PasswordGenerator from "../Assets/PasswordGenerator.png";
 import ProjectCard1 from "./ProjectCard1";
 import ProjectCard2 from "./ProjectCard2";
 import Weather from "../Assets/Weather.jpg";
+import QuadTree from "../Assets/QuadTree.png";
 
 const Work = () => {
     return (
@@ -31,6 +32,7 @@ const Work = () => {
                         description={"Check your body mass index and seek advice"} 
                         appLink={"https://check-body-mass-index.netlify.app/"} 
                         codeLink={"https://github.com/Abdullahprogramme/BMI-calculator"}
+                        detailed={"This is a BMI calculator application made using React JS. It calculates the BMI of the user and gives advice based on the BMI. The user can get advice on how to maintain their BMI and stay healthy."}
                     />
 
                     {/* Password generator */}
@@ -40,6 +42,7 @@ const Work = () => {
                         description={"Generate strong passwords using this password generator"} 
                         appLink={"https://passforge.netlify.app"} 
                         codeLink={"https://github.com/Abdullahprogramme/Password_Generator"}
+                        detailed={"This is a password generator application made using React JS. It generates strong passwords for the user. The user can select the length of the password and choose to include numbers, symbols, and uppercase letters. The user can also copy the password to the clipboard and change the theme of the application."}
                     />
                     
                     {/* Weather App */}
@@ -49,6 +52,7 @@ const Work = () => {
                         description={"Check the weather of any city using this weather app"} 
                         appLink={"https://skyswift.netlify.app"} 
                         codeLink={"https://github.com/Abdullahprogramme/Weather-App"}
+                        detailed={"This is a weather application made using React JS. It shows the weather of any city in the world. The user can search for the city and get the weather details of the city. The user can see the temperature, humidity, and weather condition of the city. Also, the user can see the weather forecast for the next 5 days."}
                     />
                 </div>
 
@@ -65,6 +69,7 @@ const Work = () => {
                         title={"Tkinter application"} 
                         description={"Test your knowledge by this interactive quiz"} 
                         codeLink={"https://github.com/Abdullahprogramme/APS-Project/"}
+                        detailed={"This is a quiz application made using Tkinter in Python. The user can test their knowledge by answering the questions in the quiz. The questions are randomly selected from a set of questions. The user can see their score at the end of the quiz."}
                     />
                     
                     {/* Password manager */}
@@ -73,6 +78,7 @@ const Work = () => {
                         title={"Password manager"} 
                         description={"Store your passwords securely by this password manager"} 
                         codeLink={"https://github.com/Abdullahprogramme/additonal-programs/blob/main/password%20manager.py"}
+                        detailed={"This is a password manager application made using Python. The user can store their passwords securely in the password manager. The user can add, view, and delete passwords. The passwords are stored in a text file."}
                     />
                     
                     {/* Pygame application */}
@@ -81,7 +87,17 @@ const Work = () => {
                         title={"Pygame application"} 
                         description={"A retro game of space invaders using Pygame"} 
                         codeLink={"https://github.com/Abdullahprogramme/PyGame/tree/main/Space%20Invaders"}
+                        detailed={"This is a game application made using Pygame in Python. The user can play a retro game of space invaders. The user can move the spaceship and shoot the aliens. The user can see their score live on the screen."}
                     />
+
+                    {/* QuadTree application */}
+                    <ProjectCard2 
+                        image={QuadTree}
+                        title={"QuadTree application"}
+                        description={"Compress images using QuadTree algorithm in Python"} 
+                        codeLink={"https://github.com/Abdullahprogramme/DSA-Project"}
+                        detailed={"This is an image compression application made using the QuadTree algorithm in Python. The user can compress images using the QuadTree algorithm. The frontend is made using PyQT5. The user can see the download the compressed image and view the original and compressed image."}
+                        />
                 </div>
             </div>
         </div>
