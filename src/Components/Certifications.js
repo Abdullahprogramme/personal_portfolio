55c4b6d0f9c611ddadfd6af8ba391bf27d3c27c4
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-05-18 22:09:10 +0500

    Add files via upload

---
+++ src/Components/Certifications.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Certifications = () => {
+    return (
+        <div name='certifications' className="w-full min-h-screen bg-[#0a192f] text-gray-300">
+            {/* container */}
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div>
+                    <p className="text-4xl inline text-[#416D19] border-pink-300 border-b-4 font-bold">Certifications</p>
+                    <p className="py-4">/ My professional certifications. /</p>
+                </div>
+
+                <div className="w-full text-center py-8">
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <h2 className="text-2xl text-[#B7E5B4]">Python For Everybody</h2>
+                        <p className="py-4 text-[#B7E5B4]">Coursera, Sep 2022</p>
+                    </div>
+
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
+                        <h2 className="text-2xl text-[#B7E5B4]">Elements Of AI</h2>
+                        <p className="py-4 text-[#B7E5B4]">University Of helsinki, Nov 2023</p>
+                    </div>
+
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
+                        <h2 className="text-2xl text-[#B7E5B4]">Problem Solving</h2>
+                        <p className="py-4 text-[#B7E5B4]">Hackerrank, Oct 2023</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Certifications;
