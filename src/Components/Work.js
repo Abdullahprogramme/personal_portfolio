d4555a83569ca60d09aea6c6e396d6f7a5e8b965
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-06-06 18:27:59 +0500

    Update Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -8,6 +8,10 @@ import ProjectCard from "./ProjectCard";
 import Weather from "../Assets/Weather.jpg";
 import QuadTree from "../Assets/QuadTree.png";
 import Movie from "../Assets/Movie.jpg";
+import TicTacToe from "../Assets/TicTacToe.jpg";
+import StopWatch from "../Assets/StopWatch.jpg";
+import Calculator from "../Assets/Calculator.jpg";
+import QRcode from "../Assets/QRcode.jpg";
  
 const Work = () => {
     return (
@@ -108,6 +112,50 @@ const Work = () => {
                         appLink={"https://quadracompress.streamlit.app/"}
                         codeLink={"https://github.com/Abdullahprogramme/DSA-Project"}
                         detailed={"This is an image compression application made using the QuadTree algorithm in Python. The user can compress images using the QuadTree algorithm. The frontend is made using PyQT5. The app's front-end is made using streamlit. The user can see the download the compressed image and view the original and compressed image."}
+                    />
+                </div>
+
+                {/* React Native Heading */}
+                <div className="pb-4 pl-4 pt-8">
+                    <p className="text-2xl font-bold inline text-[#416D19]">React Native</p>
+                </div>
+
+                {/* React Native Projects */}
+                <div className="grid sm:grid-cols-2 pl-6 pr-6 sm:pl-2 sm:pr-2 md:grid-cols-3 gap-4">
+                    {/* Calculator */}
+                    <ProjectCard 
+                        image={Calculator}
+                        title={"Calculator"} 
+                        // description={"Perform basic arithmetic operations using this calculator"}
+                        codeLink={"https://github.com/Abdullahprogramme/PRODIGY_AD_01"}
+                        detailed={"This is a calculator application made using React Native. The user can perform basic arithmetic operations using this calculator. The user can add, subtract, multiply, and divide numbers. The user can also clear the input and delete the last digit."}
+                    />
+                    
+                    {/* QR code generator */}
+                    <ProjectCard
+                        image={QRcode}
+                        title={"QR Code Reader"}
+                        // description={"Read QR codes using this QR code reader"}
+                        codeLink={"https://github.com/Abdullahprogramme/PRODIGY_AD_05"}
+                        detailed={"This is a QR code reader application made using React Native. The user can read QR codes using this QR code reader. The user can scan the QR code using the camera of the device. The user can see the content of the QR code, wether it is a URL, text, or contact information."}
+                    />
+                    
+                    {/* Tic Tac Toe */}
+                    <ProjectCard
+                        image={TicTacToe}
+                        title={"Tic Tac Toe"}
+                        // description={"Play the classic game of tic tac toe"}
+                        codeLink={"https://github.com/Abdullahprogramme/PRODIGY_AD_04"}
+                        detailed={"This is a tic tac toe application made using React Native. The user can play the classic game of tic tac toe using this application. The user can play against the computer or another player. The user can see the winner of the game."}
+                    />
+
+                    {/* Stop Watch */}
+                    <ProjectCard
+                        image={StopWatch}
+                        title={"Stop Watch"}
+                        // description={"Use this stop watch to measure time intervals"} 
+                        codeLink={"https://github.com/Abdullahprogramme/PRODIGY_AD_03"}
+                        detailed={"This is a stopwatch application made using React Native. The user can measure time intervals using this stopwatch. The user can start, stop, and reset the stopwatch."}
                     />
                 </div>
             </div>
