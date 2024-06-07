86ff28be1ad7e1a418162a42db96dd24ea79f18e
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-06-07 14:54:40 +0500

    Update Education.js

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGraduationCap, FaSchool, FaUniversity } from 'react-icons/fa';
 
 const Education = () => {
     return (
@@ -11,20 +12,30 @@ const Education = () => {
                 </div>
 
                 <div className="w-full text-center py-8">
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <h2 className="text-2xl text-white">Bachelor's Degree in Computer Science</h2>
-                        <p className="py-4 text-white">Habib University, 2023 - 2027</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 flex items-center">
+                        <FaGraduationCap className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">Bachelor's Degree in Computer Science</h2>
+                            <p className="py-4 text-white">Habib University, 2023 - 2027</p>
+                        </div>
                     </div>
 
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
-                        <h2 className="text-2xl text-white">A-Levels</h2>
-                        <p className="py-4 text-white">The City School - Gulshan A-Levels, 2021 - 2023</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4 flex items-center">
+                        <FaUniversity className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">A-Levels</h2>
+                            <p className="py-4 text-white">The City School - Gulshan A-Levels, 2021 - 2023</p>
+                        </div>
                     </div>
 
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
-                        <h2 className="text-2xl text-white">O-Levels</h2>
-                        <p className="py-4 text-white">The City School - Boys Campus A, 2016 - 2021</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4 flex items-center">
+                        <FaSchool className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">O-Levels</h2>
+                            <p className="py-4 text-white">The City School - Boys Campus A, 2016 - 2021</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
