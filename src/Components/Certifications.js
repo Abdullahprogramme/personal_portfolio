024a9dbaaea548409bf3ad092d1477b89798ccdb
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-06-07 14:54:56 +0500

    Update Certifications.js

--- src/Components/Certifications.js
+++ src/Components/Certifications.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPython, FaBrain, FaPuzzlePiece } from 'react-icons/fa';
 
 const Certifications = () => {
     return (
@@ -11,19 +12,28 @@ const Certifications = () => {
                 </div>
 
                 <div className="w-full text-center py-8">
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <h2 className="text-2xl text-white">Python For Everybody</h2>
-                        <p className="py-4 text-white">Coursera, Sep 2022</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 flex items-center">
+                        <FaPython className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">Python For Everybody</h2>
+                            <p className="py-4 text-white">Coursera, Sep 2022</p>
+                        </div>
                     </div>
 
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
-                        <h2 className="text-2xl text-white">Elements Of AI</h2>
-                        <p className="py-4 text-white">University Of Helsinki, Nov 2023</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4 flex items-center">
+                        <FaBrain className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">Elements Of AI</h2>
+                            <p className="py-4 text-white">University Of Helsinki, Nov 2023</p>
+                        </div>
                     </div>
 
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4">
-                        <h2 className="text-2xl text-white">Problem Solving</h2>
-                        <p className="py-4 text-white">Hackerrank, Oct 2023</p>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 mt-4 flex items-center">
+                        <FaPuzzlePiece className="text-5xl pl-2" />
+                        <div className="text-center flex-grow">
+                            <h2 className="text-2xl text-white">Problem Solving</h2>
+                            <p className="py-4 text-white">Hackerrank, Oct 2023</p>
+                        </div>
                     </div>
 
                 </div>
