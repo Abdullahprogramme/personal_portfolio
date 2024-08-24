b2f01cf4d943eb674d730326f8a3e331146b9fd4
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-08-24 22:01:22 +0500

    Update Certifications.js

--- src/Components/Certifications.js
+++ src/Components/Certifications.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaPython, FaBrain, FaPuzzlePiece } from 'react-icons/fa';
 import { MdModelTraining } from "react-icons/md";
-import Fade from 'react-reveal/Fade'; // Importing React Reveal's Fade
 
 const Certifications = () => {
     return (
@@ -15,75 +14,61 @@ const Certifications = () => {
                 {/* Card layout */}
                 <div className="mt-10">
                     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-
-                        {/* Card 1 */}
-                        <Fade bottom>
-                            <div className="flex">
-                                <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
-                                    <div className="-mt-6">
-                                        <div>
-                                            <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
-                                                <FaPython className="text-white text-3xl" />
-                                            </span>
-                                        </div>
-                                        <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Python For Everybody</h3>
-                                        <p className="mt-5 text-base text-gray-400">Coursera, Sep 2022</p>
+                        <div className="flex">
+                            <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
+                                <div className="-mt-6">
+                                    <div>
+                                        <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                                            <FaPython className="text-white text-3xl" />
+                                        </span>
                                     </div>
+                                    <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Python For Everybody</h3>
+                                    <p className="mt-5 text-base text-gray-400">Coursera, Sep 2022</p>
                                 </div>
                             </div>
-                        </Fade>
+                        </div>
 
-                        {/* Card 2 */}
-                        <Fade bottom>
-                            <div className="flex">
-                                <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
-                                    <div className="-mt-6">
-                                        <div>
-                                            <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
-                                                <FaBrain className="text-white text-3xl" />
-                                            </span>
-                                        </div>
-                                        <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Elements Of AI</h3>
-                                        <p className="mt-5 text-base text-gray-400">University Of Helsinki, Nov 2023</p>
+                        <div className="flex">
+                            <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
+                                <div className="-mt-6">
+                                    <div>
+                                        <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                                            <FaBrain className="text-white text-3xl" />
+                                        </span>
                                     </div>
+                                    <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Elements Of AI</h3>
+                                    <p className="mt-5 text-base text-gray-400">University Of Helsinki, Nov 2023</p>
                                 </div>
                             </div>
-                        </Fade>
+                        </div>
 
-                        {/* Card 3 */}
-                        <Fade bottom>
-                            <div className="flex">
-                                <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
-                                    <div className="-mt-6">
-                                        <div>
-                                            <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
-                                                <FaPuzzlePiece className="text-white text-3xl" />
-                                            </span>
-                                        </div>
-                                        <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Problem Solving</h3>
-                                        <p className="mt-5 text-base text-gray-400">Hackerrank, Oct 2023</p>
+                        <div className="flex">
+                            <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
+                                <div className="-mt-6">
+                                    <div>
+                                        <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                                            <FaPuzzlePiece className="text-white text-3xl" />
+                                        </span>
                                     </div>
+                                    <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Problem Solving</h3>
+                                    <p className="mt-5 text-base text-gray-400">Hackerrank, Oct 2023</p>
                                 </div>
                             </div>
-                        </Fade>
+                        </div>
 
-                        {/* Card 4 */}
-                        <Fade bottom>
-                            <div className="flex">
-                                <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
-                                    <div className="-mt-6">
-                                        <div>
-                                            <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
-                                                <MdModelTraining className="text-white text-3xl" />
-                                            </span>
-                                        </div>
-                                        <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Microsoft Learn, Technical Training</h3>
-                                        <p className="mt-5 text-base text-gray-400">Microsoft, August 2024</p>
+                        <div className="flex">
+                            <div className="flex-grow flow-root bg-gray-800 rounded-lg px-6 pb-8 shadow-lg">
+                                <div className="-mt-6">
+                                    <div>
+                                        <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                                            <MdModelTraining className="text-white text-3xl" />
+                                        </span>
                                     </div>
+                                    <h3 className="mt-8 text-lg font-medium text-white tracking-tight">Microsoft Learn, Technical Training</h3>
+                                    <p className="mt-5 text-base text-gray-400">Microsoft, August 2024</p>
                                 </div>
                             </div>
-                        </Fade>
-
+                        </div>
                     </div>
                 </div>
             </div>
