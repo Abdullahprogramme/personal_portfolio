3c508f3eb3e23b93c954654d3cac1553d499511f
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-09-29 19:59:49 +0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,6 +4,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import {Link} from 'react-scroll';
+import {FaCode} from 'react-icons/fa';
 
 const NavBar = () => {
     const [nav, setnav] = useState(false);
@@ -21,12 +22,28 @@ const NavBar = () => {
 
     const links = ['Home', 'About', 'Education', 'Experience', 'Skills', 'Work', 'Contact']; // replace with your actual links
 
+    const CodeBlock = () => {
+        return (
+            <div className="bg-gray-800 text-white p-4 rounded-lg font-mono text-sm flex items-center">
+                <FaCode className="text-gray-500 mr-2" />
+                <pre className="whitespace-pre-wrap flex items-center m-0 p-0">
+                    <code className="flex items-center f">
+                        {`ABDULLAH TARIQ`}
+                    </code>
+                </pre>
+                <FaCode className="text-gray-500 ml-2" />
+            </div>
+        );
+    };
+    
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] bg-opacity-60 text-gray-300" style={{ zIndex: 1000 }}>
-            <div className="insta">
+            <div className=""> {/* can use 'insta' class to give the hover effect */}
                 <a href="https://www.instagram.com/abdullahtariq62/?next=%2F&hl=en" target="_blank" rel="noopener noreferrer">
                     {/* <img src={man} alt="abdullah" style={{width: '50px'}}/> */}
-                    <p className="customFontStyle text-4xl text-gray-300">AT</p>
+                    {/* <p className="customFontStyle text-4xl text-gray-300">AT</p> */}
+                    {/* <p className=" font-bold text-3xl text-gray-300 customFontStyle">ABDULLAH TARIQ</p> */}
+                    <CodeBlock />
                 </a>
             </div>
 
@@ -47,24 +64,6 @@ const NavBar = () => {
                     </li>
                 ))}
             </ul>
-
-            {/* <ul className="hidden custom:flex">
-                <li>
-                    <Link to="home" duration={500} smooth={true} className="border-animation">Home</Link>
-                </li>
-                <li>
-                    <Link to="about" duration={500} smooth={true} className="border-animation">About</Link>
-                </li>
-                <li>
-                    <Link to="skills" duration={500} smooth={true} className="border-animation">Skills</Link>
-                </li>
-                <li>
-                    <Link to="work" duration={500} smooth={true} className="border-animation">Work</Link>
-                </li>
-                <li>
-                    <Link to="contact" duration={500} smooth={true} className="border-animation">Contact</Link>
-                </li>
-            </ul> */}
 
             <div onClick={handleclick} className="custom:hidden z-10">
                 {!nav ? <FaBars /> : <FaTimes />}
