e4420881cb1d25a6b9c12574eb0370626b11284f
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-01-13 19:51:34 +0500

    Update Certifications.js

--- src/Components/Certifications.js
+++ src/Components/Certifications.js
@@ -67,7 +67,7 @@ const Certifications = () => {
 
   return (
     <div ref={ref} name="certifications" className="w-full min-h-screen bg-[#0a192f] text-gray-300">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pb-20">
         <div>
           <p className="text-4xl text-[#416D19] font-bold border-b-4 border-pink-300 inline-block">Certifications</p>
           <p className="py-4">/ My professional certifications. /</p>
